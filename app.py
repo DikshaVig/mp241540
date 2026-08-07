062730b21ab53c3203f786cd6a455198c6a7b46e
Diksha Vig <dikshavig47@gmail.com>
2026-08-07 15:42:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 app.secret_key = 'customer_segmentation_secret_key_change_in_production'
 
-# Import local helper files
-from predictor import predict_segment
+
+from utiles.predictor import predict_segment
 from recommendation import get_strategy
 from database import get_db, init_db
 
